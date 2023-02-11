d5952efa9c8728a5458eb91a193bc9a2c2c3e7b0
wn <1303682773@qq.com>
2023-02-11 21:58:00 +0800

    feat:react源码学习方式

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typist from 'react-typist';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -7,18 +8,29 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
+const typistProps = {
+  cursor:{
+    show: true,
+    blink: true,
+    element: '|',
+    hideWhenDone: true,
+    hideWhenDoneDelay: 0,
+  }
+}
+
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">博客</h1>
+        <p className="hero__subtitle">前端Blog</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+              <Typist {...typistProps}>
+                前端，从入门到放弃系列 😆
+              </Typist>
           </Link>
         </div>
       </div>
